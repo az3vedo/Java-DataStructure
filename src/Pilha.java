7c681c931d50b025093409663e5a4140b0b6c307
Gabriel Souza <azevedoedit@gmail.com>
2023-12-07 19:07:26 -0300

    Add stack folder and its components

---
+++ src/Pilha.java
@@ -0,0 +1,49 @@
+public class Pilha {
+	
+	private No refNoEntradaPilha;
+
+	public Pilha() {
+		this.refNoEntradaPilha = null;
+	}
+	
+	public boolean isEmpty() {
+		 return (refNoEntradaPilha == null);	
+	}
+	
+	public void push(No novoNo) { 
+		No auxiliar = refNoEntradaPilha;
+		novoNo.setRefNo(auxiliar);
+		refNoEntradaPilha = novoNo;	
+	}
+	
+	public No pop() {
+		if(isEmpty()) {
+			return null;
+		} else {
+			No noPopped = refNoEntradaPilha;
+			refNoEntradaPilha = refNoEntradaPilha.getRefNo();
+			return noPopped;
+		}
+		
+	}
+	
+	public No top() {
+		return refNoEntradaPilha;
+	}
+	
+	@Override
+	public String toString() {
+		String stringRetorno = "--------------------\n";
+		stringRetorno += "\tPilha\n";
+		stringRetorno += "--------------------\n";
+		
+		No noAuxiliar = refNoEntradaPilha;
+		
+		while(noAuxiliar != null) {
+			stringRetorno += noAuxiliar.toString() + "\n";
+			noAuxiliar = noAuxiliar.getRefNo();
+		}
+		stringRetorno += "====================\n";
+		return stringRetorno;
+	}
+}
